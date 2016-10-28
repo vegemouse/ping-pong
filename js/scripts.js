287f77e93d5b40b6b5344b34c124330b20619dae
Max Cady <maxwell.josef@gmail.com>
2016-10-27 19:00:30 -0700

    add function to display each number up to and including inputted number in a list item

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
+var pingpong = function(number) {
+  var numberCount = 0;
+  var numbersList = [];
+  for (var i = 0; i < number; i++) {
+    numberCount++;
+    numbersList.push(numberCount);
+  }
+  return numbersList;
+  numbersList = [];
+}
+
+// Front end
 $(function() {
   $("#pingpong").submit(function(event){
     event.preventDefault();
     var input = $("input").val();
-    console.log(input);
     var result = pingpong(input);
-    $("#output").
+    $("#output").empty();
+    result.forEach(function(number) {
+      $("#output").append("<li>" + number + "</li>");
+    })
   })
 })
